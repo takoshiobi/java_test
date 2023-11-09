f315e5ca8daf570ac9876f426f201bda27b1a7c0
Yulia Scherbakova <yscherbakova@dpd.ru>
2023-11-09 16:43:53 +0300

    Stream API tasks

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,58 @@
-import Utils.Tasks;
+import Utils.StreamAPITasks;
 
-import java.math.BigInteger;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        BigInteger task1_res = Tasks.addition();
-        System.out.printf("Addition of big integers result: %d%n", task1_res);
-
-        Integer task2_res = Tasks.division();
-        System.out.printf("Division result: %d%n", task2_res);
+        ArrayList<Integer> numbers = IntStream.generate(() -> new Random().nextInt(200)).limit(100).collect(ArrayList::new, List::add, List::addAll);
 
         try {
-            String task3_res = Tasks.reverseString("level");
-            System.out.printf("Reversed string result: %s%n", task3_res);
+            System.out.printf("Array length: %s%n", StreamAPITasks.arrayLength(numbers));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
         try {
-            String task4_res = Tasks.removeDuplicatesFromString("aabvvvcc");
-            System.out.printf("Removed duplicates from string result: %s%n", task4_res);
+            System.out.printf("Get first ten elements: %s%n", StreamAPITasks.firstTen(numbers));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
         try {
-            Integer[] arr = {1, 2, 3, 4};
-            Integer[] task5_res = Tasks.reverseArray(arr);
-            System.out.printf("Reversed array result: %s%n", Arrays.toString(task5_res));
+            System.out.printf("Get last ten elements: %s%n", StreamAPITasks.lastTen(numbers));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
         try {
-            int[][] twoDArray = {{1, 2}, {2, 3}};
-            int task6_res = Tasks.twoDArraySum(twoDArray);
-            System.out.printf("Two dimensional array sum result: %s%n", task6_res);
+            System.out.printf("Multiply each element by ten: %s%n", StreamAPITasks.multiplyTen(numbers));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.printf("Add 2 to odd numbers: %s%n", StreamAPITasks.oddAddTwo(numbers));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.printf("Sum of array elements: %s%n", StreamAPITasks.arraySum(numbers));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.printf("Check if array contains numbers that are greater than 100: %s%n", StreamAPITasks.containsIntGreaterThan100(numbers));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.printf("Convert array to map: %s%n", StreamAPITasks.convertToMap(numbers));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
